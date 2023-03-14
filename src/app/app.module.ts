2d99412b0336bfc8f042dffb64895b7534c528b1
mfort89 <118136193+mfort89@users.noreply.github.com>
2023-03-14 15:22:30 -0300

    14/3
    eliminando social

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,25 +5,29 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoApComponent } from './components/logo-ap/logo-ap.component';
-import { SocialComponent } from './components/social/social.component';
+
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercadeComponent } from './components/acercade/acercade.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
 import { EducacionComponent } from './components/educacion/educacion.component';
 import { NgCircleProgressModule } from 'ng-circle-progress';
 import { HandSComponent } from './components/hand-s/hand-s.component';
+import { ProyectoComponent } from './components/proyecto/proyecto.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     LogoApComponent,
-    SocialComponent,
+
     BannerComponent,
     AcercadeComponent,
     ExperienciaComponent,
     EducacionComponent,
-    HandSComponent
+    HandSComponent,
+    ProyectoComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
